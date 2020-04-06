b40cbc4cb6ca9f8719bdde7c63e86096e94bbc83
lazaro <lazaromello14@gmail.com>
2020-04-06 14:24:09 -0300

    changing

--- app.js
+++ app.js
@@ -1,63 +1,61 @@
 window.addEventListener('load', () => {
 
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
-    let temperatureSection = document.querySelector('temperature');
-    let temperatureSpan = document.querySelector('temperature span');
+   let temperatureDescription = document.querySelector('.temperature-description');
+   let temperatureDegree = document.querySelector('.temperature-degree');
+   let locationTimezone = document.querySelector('.location-timezone');
+   let temperatureSection = document.querySelector('temperature');
+   let temperatureSpan = document.querySelector('temperature span');
 
-    if (navigator.geolocation) {
+   if (navigator.geolocation) {
 
-        navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
+      navigator.geolocation.getCurrentPosition(position => {
+         console.log(position)
 
-            let long = position.coords.longitude;
-            let lat = position.coords.latitude;
-            const proxy = 'https://cors-anywhere.herokuapp.com/'
-            const api = `${proxy}https://api.darksky.net/forecast/c867ea8753c9f3e3149c850144931fb8/${lat},${long}`;
+         let long = position.coords.longitude;
+         let lat = position.coords.latitude;
+         const proxy = 'https://cors-anywhere.herokuapp.com/'
+         const api = `${proxy}https://api.darksky.net/forecast/c867ea8753c9f3e3149c850144931fb8/${lat},${long}`;
 
-            fetch(api)
-                .then(response => {
+         fetch(api)
+            .then(response => {
 
-                    return response.json();
+               return response.json();
 
-                })
-                .then(data => {
+            })
+            .then(data => {
 
-                    console.log(data);
-                    const {
-                        temperature,
-                        summary,
-                        icon
-                    } = data.currently;
+               console.log(data);
+               const {
+                  temperature,
+                  summary,
+                  icon
+               } = data.currently;
 
-                    // set DOM Elements from API
-                    temperatureDegree.textContent = temperature;
-                    temperatureDescription.textContent = summary;
-                    locationTimezone.textContent = data.timezone;
-                    let celsius = (temperature - 32) * (5 / 9)
+               // set DOM Elements from API
+               temperatureDegree.textContent = temperature;
+               temperatureDescription.textContent = summary;
+               locationTimezone.textContent = data.timezone;
+               let celsius = (temperature - 32) * (5 / 9)
 
-                    //set icon
-                    setIcons(icon, document.querySelector('.icon'));
+               //set icon
+               setIcons(icon, document.querySelector('.icon'));
 
-                    //Change temperature  to Celsius/Farenheit
-                    temperatureSection.addEventListener('click', () => {
+               //Change temperature  to Celsius/Farenheit
+               temperatureSection.addEventListener('click', () => {
 
-                        if (temperatureSpan.textContent === 'F') {
+                  if (temperatureSpan.textContent === 'F') {
 
-                            temperatureSpan.textContent = 'C';
-                            temperatureDegree.textContent = Math.floor(celsius);
+                     temperatureSpan.textContent = 'C';
+                     temperatureDegree.textContent = Math.floor(celsius);
 
-                        } else {
-                            
-                            temperatureSpan.textContent = 'F';
-                            temperatureDegree.textContent = temperature;
+                  } else {
 
-                        }
-                    })
-                })
-        })
-    }
+                     temperatureSpan.textContent = 'F';
+                     temperatureDegree.textContent = temperature;
+
+                  }
+               })
+            })
+      })
+   }
 })
-
- 
